d42c5300eca6c9ef12b00c7fcd981254424a98a8
Josec690 <joseclaudiley@gmail.com>
2026-05-05 17:30:25 -0300

    commit 1

--- src/main/java/br/org/museu/hardware/model/Equipamento.java
+++ src/main/java/br/org/museu/hardware/model/Equipamento.java
@@ -1,15 +1,21 @@
-package br.org.museu.hardware.model;
+package br.org.museu.hardware.model; // Certifique-se de que o pacote esteja correto e corresponda à estrutura do seu projeto
 
-import jakarta.persistence.*;
-import lombok.Data;
+import jakarta.persistence.*; // Importando as anotações do JPA para definir a entidade e suas propriedades
+import lombok.Data; // Importando a anotação do Lombok para gerar getters e setters automaticamente, se você estiver usando o Lombok em seu projeto
 
-@Entity
-@Table(name = "Equipamentos")
+@Entity // Indica que esta classe é uma entidade JPA e será mapeada para uma tabela no banco de dados
+@Table(name = "EQUIPAMENTOS") // Especifica o nome da tabela no banco de dados para esta entidade, seguindo a convenção do DER que você forneceu
 @Data // Se você instalou o Lombok, isso gera getters e setters automaticamente
 
-public class Equipamento {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long pk_id_equipo; //
+public class Equipamento { // Definindo a classe Equipamento, que representa a entidade de equipamentos no sistema
+    @Id // Indica que este campo é a chave primária da tabela
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Especifica que o valor da chave primária será gerado automaticamente pelo banco de dados, usando a estratégia de identidade
+    private Long pk_id_equipo; // Seguindo seu DER // Definindo o campo pk_id_equipo como a chave primária da tabela, do tipo Long
+    private String modelo;
+    private int ano;
+    private String geracao;
+
+    // Depois adicionarei os relacionamentos com as outras entidades, como Fabricante, TipoEquipamento, etc.
+
 
 }
